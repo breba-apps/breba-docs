1a24c1654ec397cad36097b15b4287a530e00fc5
Yason Khaburzaniya <yason@hey.com>
2024-12-12 14:43:04 -0800

    add some integration tests for CommandAgent

--- tests/integration/test_command_agent.py
+++ tests/integration/test_command_agent.py
@@ -15,18 +15,40 @@ def openai_agent():
     with OpenAIAgent() as agent:
         yield agent
 
+@pytest.fixture
+def agent(openai_agent):
+    with ContainerCommandExecutor.executor_and_new_container(openai_agent) as command_executor:
+        with command_executor.session() as session:
+            yield CommandAgent(session)
 
 @pytest.mark.integration
-def test_container_command_executor(mocker, openai_agent):
+def test_typo_command_with_fix(agent):
     commands = ['python3 -m venv .venv', 'cd .venv',
                 'source bin/activote']
     messages = []
-    with ContainerCommandExecutor.executor_and_new_container(OpenAIAgent()) as command_executor:
-        with command_executor.session() as session:
-            agent = CommandAgent(session)
-            for command in commands:
-                messages = agent.invoke(command)["messages"]
-    command_json = json.loads(messages[-1].content)
-    command_report = CommandReport(**command_json)
-    assert command_report.command == "source bin/activate"
 
+    for command in commands:
+        messages = agent.invoke(command)["messages"]
+
+    command_report = CommandReport.from_string(messages[-1].content)
+
+    assert command_report.command == "source bin/activote"
+    assert command_report.improved_command == "source bin/activate"
+
+@pytest.mark.integration
+def test_typo_command_without_fix(agent):
+    command = 'source bin/someabitch'
+    messages = agent.invoke(command)["messages"]
+    command_report = CommandReport.from_string(messages[-1].content)
+
+    assert command_report.command == "source bin/someabitch"
+    assert command_report.improved_command is None
+
+@pytest.mark.integration
+def test_command_with_missing_setup(agent):
+    command = 'cd my_project'
+    messages = agent.invoke(command)["messages"]
+    command_report = CommandReport.from_string(messages[-1].content)
+
+    assert command_report.command == "cd my_project"
+    assert command_report.improved_command is None

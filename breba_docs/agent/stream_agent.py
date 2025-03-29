328acf84b4bcee1d17ca31cd182cb86f47e73ae0
Yason Khaburzaniya <yason@hey.com>
2025-03-29 06:40:31 -0700

    streaming agent streaming tool (with bugs)

--- breba_docs/agent/stream_agent.py
+++ breba_docs/agent/stream_agent.py
@@ -1,38 +1,71 @@
 from typing import TypedDict
 
 from dotenv import load_dotenv
+from langchain_core.tools import tool
 from langchain_openai import ChatOpenAI
 from langgraph.constants import START
-from langgraph.graph import StateGraph
+from langgraph.graph import StateGraph, MessagesState
+from langgraph.prebuilt import ToolNode
 
 load_dotenv()
 
-llm = ChatOpenAI(model="gpt-4o-mini")
+llm = ChatOpenAI(model="gpt-4o")
 
 
-class State(TypedDict):
+class State(MessagesState):
     topic: str
-    joke: str
+    answer: str
+
+
+@tool
+def refine_prompt(prompt: str):
+    """Improves the prompt
+    Args:
+        prompt (str): original user prompt
+    """
+    print("Tool calling:")
+    llm_response = llm.invoke(
+        [
+            {"role": "user", "content": f"Improve the following prompt make sure the prompt includes number of "
+                                        f"words to generate. The number of words should not exceed 50: {prompt}"}
+        ]
+    )
+    return llm_response
+
+
+tools = [refine_prompt]
+tool_node = ToolNode(tools)
+llm_with_tools = llm.bind_tools(tools)
 
 
 def refine_topic(state: State):
-    return {"topic": state["topic"] + " and cats"}
-
-def generate_joke(state: State):
-    llm_response = llm.invoke(
+    llm_response = llm_with_tools.invoke(
         [
-            {"role": "user", "content": f"Generate 1500 words about general relativity and gravity"}
+            {"role": "system", "content": f"Improve the prompt by using refine_prompt tool. Use the exact user prompt as input parameter to the tool."},
+            {"role": "user", "content": f"general relativity and gravity"}
         ]
     )
-    return {"joke": llm_response.content}
+    return {"topic": llm_response.content, "messages": [llm_response]}
+
+def generate_answer(state: State):
+    print("\nGenerating answer")
+    llm_response = llm.invoke(
+        [
+            {"role": "user", "content": "provide answer in markdown format"},
+            {"role": "user", "content": state["messages"][-1].content}
+        ]
+    )
+    return {"answer": llm_response.content}
 
 
 graph = (
     StateGraph(State)
     .add_node(refine_topic)
-    .add_node(generate_joke)
+    .add_node("action", tool_node)
+    .add_node(generate_answer)
     .add_edge(START, "refine_topic")
-    .add_edge("refine_topic", "generate_joke")
+    .add_edge("refine_topic", "action")
+    .add_edge("action", "generate_answer")
     .compile()
 )
 
@@ -41,4 +74,4 @@ for message_chunk, metadata in graph.stream(
     stream_mode="messages",
 ):
     if message_chunk.content:
-        print(message_chunk.content, end="")
+        print(message_chunk.content, end="", flush=True)
